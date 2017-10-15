4c5d76a7fc37084a019986a53a0a24a54b6e5c35
Zavorotyuk <v.zavorotyuk@gmail.com>
2017-10-16 02:51:45 +0300

    Add products list

--- src/app/modules/products/products.component.ts
+++ src/app/modules/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../../shared/shared.service';
 
 @Component({
   selector: 'app-products',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  public productsList = [];
+
+  constructor(private _shared: SharedService) {
+    this._shared.getProductsList().subscribe(
+      data => this.productsList = data,
+      err => err
+    )
+  }
 
   ngOnInit() {
   }
